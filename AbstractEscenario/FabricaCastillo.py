bb7b0debe0044680aa39fe6d931953dc4d373e58
brayanpasa99 <brayanpasa99@hotmail.com>
2019-03-05 16:11:13 -0500

    Abstract Escenarios

---
+++ AbstractEscenario/FabricaCastillo.py
@@ -0,0 +1,13 @@
+from AbstractEscenario.FabricaPrincipalE import FabricaPrincipalE
+
+
+class FabricaCastillo(FabricaPrincipalE):
+
+    def crearIElfo(self):
+        pass
+
+    def crearIGuerrero(self):
+        pass
+
+    def crearIOrco(self):
+        pass
---
+++ BuilderEscenario/IzquierdaJug.py
@@ -0,0 +1,11 @@
+from BuilderEscenario.BuilderAbstractE import BuilderAbstractE
+
+
+class IzquierdaJug(BuilderAbstractE):
+
+    def getIconos(self):
+        return
+
+    def getSCastillo(self):
+        pass
+
