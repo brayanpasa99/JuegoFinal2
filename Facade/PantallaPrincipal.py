94f63a49c7fc66cc7e1ee57798e9d201d9a318da
brayanpasa99 <brayanpasa99@hotmail.com>
2019-03-06 03:06:42 -0500

    Update

--- Facade/PantallaPrincipal.py
+++ Facade/PantallaPrincipal.py
@@ -15,54 +15,56 @@ class PantallaPrincipal():
     def mostrarpantalla(self):
         pygame.init()
 
-    ventana = pygame.display.set_mode(DIMENSIONES)
-    pygame.display.set_caption("Mundo de razas")
+        ventana = pygame.display.set_mode(DIMENSIONES)
+        pygame.display.set_caption("Mundo de razas")
 
-    image_Fondo = pygame.transform.scale(pygame.image.load('Imagenes/Fondos/FondoPrincipal.png'), DIMENSIONES);
+        image_Fondo = pygame.transform.scale(pygame.image.load('Imagenes/Fondos/FondoPrincipal.png'), DIMENSIONES)
 
-    #Botón de jugar
-    boton_jugar = pygame.transform.scale(pygame.image.load('Imagenes/Botones/Jugar.png'), (300, 100))
-    rect_boton_jugar = boton_jugar.get_rect()
-    rect_boton_jugar.topleft = (400, 200)
+        Fuente = pygame.font.SysFont("Arial", 30)
 
-    botones = []
+        #Botón de jugar
+        boton_jugar = pygame.transform.scale(pygame.image.load('Imagenes/Botones/Jugar.png'), (300, 100))
+        rect_boton_jugar = boton_jugar.get_rect()
+        rect_boton_jugar.topleft = (400, 200)
 
-    botones.append({'nombre': "BotonJugar", 'imagen': boton_jugar, 'rect': rect_boton_jugar, 'on_click': False})
+        botones = []
+
+        botones.append({'nombre': "BotonJugar", 'imagen': boton_jugar, 'rect': rect_boton_jugar, 'on_click': False})
 
 
-    teclas = pygame.key.get_pressed()
-    reloj = pygame.time.Clock()
+        teclas = pygame.key.get_pressed()
+        reloj = pygame.time.Clock()
 
-    while True:
+        while True:
 
-        ventana.blit(image_Fondo, (0, 0))
+            ventana.blit(image_Fondo, (0, 0))
 
-        DibujarBotones().dibujar_botones(botones, ventana)
+            DibujarBotones().dibujar_botones(botones, ventana)
 
-        for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
+            for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        sys.exit()
 
-                if event.type == MOUSEBUTTONDOWN:
-                    mouse = event.pos
-                    for boton in botones:
-                        boton['on_click'] = boton['rect'].colliderect(mouse[0], mouse[1], 1, 1)
-                        if boton['on_click']:
-                            if boton['nombre'] == 'BotonJugar':
-                                Jugadores = []
-                                PantallaJuego().batalla(ElegirRaza().pide_raza(1, False), ElegirRaza().pide_raza(2, True))
+                    if event.type == MOUSEBUTTONDOWN:
+                        mouse = event.pos
+                        for boton in botones:
+                            boton['on_click'] = boton['rect'].colliderect(mouse[0], mouse[1], 1, 1)
+                            if boton['on_click']:
+                                if boton['nombre'] == 'BotonJugar':
+                                    Jugadores = []
+                                    PantallaJuego().batalla(ElegirRaza().pide_raza(1, False), ElegirRaza().pide_raza(2, True))
 
-                            else:
-                                print "ERROR GRAVISIMO IMPERDONABLE"
+                                else:
+                                    print "ERROR GRAVISIMO IMPERDONABLE"
 
-        if event.type == MOUSEBUTTONUP:
-            for boton in botones:
-                boton['on_click'] = False
+            if event.type == MOUSEBUTTONUP:
+                for boton in botones:
+                    boton['on_click'] = False
 
-        #Jugador1.dibujar(ventana, True, False, False)
-        #Jugador1.update()
+            #Jugador1.dibujar(ventana, True, False, False)
+            #Jugador1.update()
 
-        pygame.display.flip()
-        pygame.display.update()
-#reloj.tick(60)
+            pygame.display.flip()
+            pygame.display.update()
+            #reloj.tick(60)
