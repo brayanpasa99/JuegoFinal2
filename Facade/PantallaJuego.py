03aa604471e2187391b2ead663319b02e952c365
brayanpasa99 <brayanpasa99@hotmail.com>
2019-03-05 21:26:54 -0500

    Update Facade

---
+++ Facade/PantallaJuego.py
@@ -0,0 +1,44 @@
+import sys
+
+import pygame
+
+DIMENSIONES = (1100, 600)
+COLOR_TEXTO = (243, 255, 0)
+DICONOS = (200, 200)
+
+class PantallaJuego():
+
+    def batalla(self):
+        pygame.init()
+
+        ventana = pygame.display.set_mode(DIMENSIONES)
+        pygame.display.set_caption("Campo de Batalla")
+
+        image_Fondo = pygame.transform.scale(pygame.image.load('Imagenes/Fondos/Fondo4.jpg'), (1000, 500));
+
+
+        reloj = pygame.time.Clock()
+
+
+        while True:
+
+            teclas = pygame.key.get_pressed()
+            ventana.blit(image_Fondo, (0, 0))
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == K_1:
+                        Jugador1.crearPersonaje(1)
+                    if event.key == K_2:
+                        Jugador1.crearPersonaje(2)
+                    if event.key == K_3:
+                        Jugador1.crearPersonaje(3)
+
+            Jugador1.dibujar(ventana)
+            Jugador1.update()
+
+        pygame.display.update()
+        #reloj.tick(27)
